4afa074193d24f0889146e544a5bb3183fde205b
stkirk <shaanekirk@gmail.com>
2021-10-15 14:56:55 -0400

    server working with catchall and error handling middleware

--- api/server.js
+++ api/server.js
@@ -1,7 +1,28 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
 // DO YOUR MAGIC
+server.use(express.json());
 
-module.exports = server
+// server.use('/api/cars', carsRouter)
+
+server.get("/", (req, res, next) => {
+  res.send(`
+        <h1>Welcome to Cars API</h1>
+        <h4>Use: /api/cars</h4>
+    `);
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
+//error handling middleware
+server.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Something went wrong" });
+});
+
+module.exports = server;
