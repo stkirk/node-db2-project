0e679d9a4cafafa6520a6336a170848a4f4c7236
stkirk <shaanekirk@gmail.com>
2021-10-15 16:07:47 -0400

    cars router scaffolded

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.send("GET cars working...");
+});
+
+router.get("/:id", (req, res, next) => {
+  res.send("GET cars by ID working...");
+});
+
+router.post("/", (req, res, next) => {
+  res.send("POST car working...");
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const carsRouter = require("./cars/cars-router");
 
 const server = express();
 
 // DO YOUR MAGIC
 server.use(express.json());
 
-// server.use('/api/cars', carsRouter)
+server.use("/api/cars", carsRouter);
 
 server.get("/", (req, res, next) => {
   res.send(`
